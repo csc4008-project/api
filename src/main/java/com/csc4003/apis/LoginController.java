bbd6ddf4dc2c41e6d9212f38b1af5f3c67858d51
40237845 <jandrews20@qub.ac.uk>
2022-11-21 01:35:31 +0000

    Example of JWT auth working on Login API. Will need expanded and tidied as we add more APIs to the mix but shows a simple implementation to begin

--- src/main/java/com/csc4003/apis/LoginController.java
+++ src/main/java/com/csc4003/apis/LoginController.java
@@ -1,18 +1,65 @@
 package com.csc4003.apis;
 
-import com.csc4003.apis.models.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.csc4003.apis.models.Employee;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+import io.jsonwebtoken.io.*;
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     @CrossOrigin
-    @GetMapping("/login")
-    public User getUser(@RequestParam(value = "userId") String userId)
-    {
-        return new User(userId, "Test User");
+    @RequestMapping(value ="/login", method = RequestMethod.POST)
+    public Map<String, String> login(@RequestBody Map<String, Object> json) {
+        if(json.get("email").equals("test123") && json.get("password").equals("test123")) {
+            HashMap<String, String> response = new HashMap<>() {
+                {
+                    put("accessToken", generateJWT(json.get("email").toString()));
+                    put("email", json.get("email").toString());
+                    put("name", "Test User");
+                }};
+            return response;
+        }
+        return new HashMap<>();
     }
+
+    @CrossOrigin
+    @PostMapping("/register")
+    public Employee register() {
+        return new Employee();
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/updateAccount", method = RequestMethod.POST)
+    public Map<String, String> updateAccount(@RequestBody Map<String, Object> json, @RequestHeader(HttpHeaders.AUTHORIZATION) String auth ) {
+        if(authJWT(auth.split(" ")[1], json.get("email").toString())) {
+            HashMap<String, String> response = new HashMap<>() {
+                {
+                    put("accessToken", auth.split(" ")[1]);
+                    put("email", json.get("email").toString());
+                    put("name", json.get("name").toString());
+                }};
+            return response;
+        }
+
+        return new HashMap<>();
+    }
+
+    private String generateJWT(String email) {
+        return Jwts.builder().setSubject(email).signWith(key).compact();
+    }
+
+    private boolean authJWT(String jwt, String email) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody().getSubject().equals(email);
+    }
+
 }
---
+++ src/main/java/com/csc4003/apis/models/Employee.java
@@ -0,0 +1,72 @@
+package com.csc4003.apis.models;
+
+public class Employee {
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String occupation;
+
+    public Employee(String employeeId, String firstName, String lastName, String email, String password, String occupation) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.occupation = occupation;
+    }
+
+    public Employee() {
+
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
