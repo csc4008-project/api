13e7239ec525f6f8e533a760de56ffaf09b0a734
Bartosz <bgorecki01@qub.ac.uk>
2022-11-24 11:44:07 +0000

    Removed some unnecessary examples from last push, new minor examples added to show new approach to getting data back.

--- src/main/java/com/csc4003/apis/ApIsApplication.java
+++ src/main/java/com/csc4003/apis/ApIsApplication.java
@@ -1,6 +1,8 @@
 package com.csc4003.apis;
 
+import com.csc4003.apis.Services.AttendeeService;
 import com.csc4003.apis.Services.BookingService;
+import com.csc4003.apis.models.Booking;
 import com.csc4003.apis.models.Employee;
 import com.csc4003.apis.Services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +26,8 @@ public class ApIsApplication {
     private EmployeeService employeeService;
     @Autowired
     private BookingService bookingService;
+    @Autowired
+    private AttendeeService attendeeService;
 
     public static void main(String[] args) {
         SpringApplication.run(ApIsApplication.class, args);
@@ -35,37 +40,12 @@ public class ApIsApplication {
         employeeService.addEmployee(emp);
         */
 
-        /* ------- Testing getting a query back from database - check BookingDepostiory: String findRoomBookingById(int bookingId);
-           ------- below is an example of how to store the returned string from query result, split each field returned and add it to an array.
-           ------- Could be useful for future reference with controllers
+        // ------- Testing outputting some booking data
+        //String buildingName = bookingService.findBookingDetailsById(1).getRoom().getRoomNumber();
+        //return ""+bookingService.findBookingDetailsById(1).getEmployee().getEmployeeId();
 
-        String[] elements = bookingService.findRoomBookingById(1).split(",");
-
-        List<String> fixedLengthList = Arrays.asList(elements);
-
-        ArrayList<String> listOfString = new ArrayList<String>(fixedLengthList);
-
-        ArrayList<String> columnNames = new ArrayList<>();
-        columnNames.add("Booking ID: ");
-        columnNames.add("Room Name: ");
-        columnNames.add("Room Type: ");
-        columnNames.add("Floor Number: ");
-        columnNames.add("Floor Name: ");
-        columnNames.add("Building Name: ");
-        columnNames.add("Start Time: ");
-        columnNames.add("End Time: ");
-        columnNames.add("Duration: ");
-
-        String output = "";
-        for (int i=0; i < columnNames.size(); i++){
-            output += columnNames.get(i);
-            output += listOfString.get(i);
-            output += ", \n";
-        }
-
-        return output;
-        */
-
+        // ------- Testing outputting some booking attendee data
+        //return attendeeService.findAttendeesByBooking(1).get(0).getEmployeeEmail() + " and + attendeeService.findAttendeesByBooking(1).get(1).getEmployeeEmail();
 
         return String.format("Hello %s!", name);
 
