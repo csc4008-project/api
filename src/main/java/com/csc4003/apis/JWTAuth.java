90943b132705cb93362b8d58c2139847e2e404bf
40237845 <jandrews20@qub.ac.uk>
2022-12-04 20:48:09 +0000

    jwt expiration date set and exception handling added

--- src/main/java/com/csc4003/apis/JWTAuth.java
+++ src/main/java/com/csc4003/apis/JWTAuth.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
 
 public class JWTAuth {
@@ -12,18 +14,35 @@ public class JWTAuth {
     private static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public static String generateJWT() {
-        return Jwts.builder().setSubject("auth").signWith(key).compact();
+        Date dt = new Date();
+        LocalDateTime.from(dt.toInstant()).plusDays(1);
+
+        return Jwts.builder().setSubject("auth").setExpiration(dt).signWith(key).compact();
     }
     public static String generateJWTWithEmail(String email) {
-        return Jwts.builder().setSubject("auth").claim("email", email).signWith(key).compact();
+        Date dt = new Date();
+        LocalDateTime.from(dt.toInstant()).plusDays(1);
+
+        return Jwts.builder().setSubject("auth").claim("email", email).setExpiration(dt).signWith(key).compact();
     }
 
     public static boolean authJWT(String jwt) {
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody().getSubject().equals("auth");
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody().getSubject().equals("auth");
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
 
     public static boolean authJWTWithEmail(String jwt, String email) {
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody().getSubject().equals(email);
+
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody().getSubject().equals(email);
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
 
     public static String getEmailFromJWT(String jwt) {
